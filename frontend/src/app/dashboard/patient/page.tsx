84d4b497c41a611ffe8e98675e246bb3a24c4c8a
gfavara97 <g.favara@beehivesud.it>
2025-09-04 10:17:24 +0200

    refactor: update User and MedicalRecord types and streamline data fetching logic

--- frontend/src/app/dashboard/patient/page.tsx
+++ frontend/src/app/dashboard/patient/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import PatientClient from "./patientClient";
 
-interface User = {
+type User = {
     id: number;
     username: string;
     email: string;
@@ -12,18 +12,16 @@ interface User = {
     role: string;
 };
 
-interface MedicalRecord = {
+type MedicalRecord = {
     id: number;
     description: string;
-    date: string; // oppure Date se lo converti
+    date: string;
 };
 
 export default function Page() {
-
-    const [data, setData] = useState<User | null>(null);
-    const [error, setError] = useState<string | null>(null);
-    const [medical, setMedical] = useState<MedicalRecord[]>([]);
     const [user, setUser] = useState<User | null>(null);
+    const [medical, setMedical] = useState<MedicalRecord[]>([]);
+    const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
 
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -71,40 +69,4 @@ export default function Page() {
     if (!user) return <p>Nessun utente trovato</p>;
 
     return <PatientClient medicalrecord={medical} userData={user} />;
-                const res = await axios.get('http://localhost:3001/api/auth/userID', { withCredentials: true });
-                setData(res.data);
-                fetchMedicalRecord(res.data.id);
-                LoggedUserData(res.data.id);
-            } catch (err) {
-                console.error("errore:", err);
-                setError("401 - Non Autorizzato");
-            }
-        }
-        async function fetchMedicalRecord(id: number) {
-            try {
-                const risposta = await axios.get(`http://localhost:3001/api/medical/patient/${id}`, { withCredentials: true });
-                setMedical(risposta.data)
-            } catch (err) {
-                console.error("errore:", err);
-                setError("401 - Non Autorizzato");
-            }
-        }
-        async function LoggedUserData(id: number) {
-            try {
-                const risuser = await axios.get(`http://localhost:3001/api/users/${id}`, { withCredentials: true });
-                setUser(risuser.data)
-            } catch (err) {
-                console.error("errore:", err);
-                setError("401 - Non Autorizzato");
-            }
-        }
-
-        LoggedUserID();
-    }, []);
-    console.log(data)
-    console.log(medical)
-    if (error) return <p style={{ color: "red" }}>{error}</p>;
-    if (!data) return <p>Caricamento…</p>;
-
-    return <PatientClient medicalrecord={medical} userData={user} />
 }
