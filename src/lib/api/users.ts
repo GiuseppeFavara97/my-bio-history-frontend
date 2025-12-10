5f93fb4b06c555eced2b6c27e47f0f0c6098ba47
gfavara97 <giuseppe.favara1997@gmail.com>
2025-12-10 14:39:22 +0100

    modificato nome metodo da delete a softddelete

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -5,4 +5,4 @@ export const getUsers = () => api.get("/users").then(res => res.data);
 export const getUserById = (id: string) => api.get(`/users/${id}`).then(res => res.data);
 export const createUser = (data: any) => api.post("/users/create", data).then(res => res.data);
 export const updateUser = (id: number, data: Partial<User>) => api.patch(`/users/update/${id}`).then(res => res.data);
-export const deleteUser = (id: number) => api.delete(`/users/softDelete/${id}`).then(res => res.data);
+export const softDelete = (id: number) => api.delete(`/users/softDelete/${id}`).then(res => res.data);
