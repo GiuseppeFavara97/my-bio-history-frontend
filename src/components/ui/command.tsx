dce240c51f8c831eaedac6a4fec66eae0b3f0ae9
Francytp <francytp@yahoo.it>
2025-12-15 17:59:56 +0100

    Aggiornati componenti UI

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Command as CommandPrimitive } from "cmdk"
+import * as CommandPrimitive from "cmdk"
 import { SearchIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
